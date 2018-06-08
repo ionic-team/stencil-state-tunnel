1238eee6e099101314b076be3f85ae2a125ec733
Josh Thomas <jthoms1@gmail.com>
2018-06-08 15:04:30 -0500

    chore(): updated js docs path.

--- docs/sw.js
+++ docs/sw.js
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "f9c3345c9aa582b44f22bd04bccf8b6a"
+    "revision": "40f551f1de6a82302e64210ebe2486b8"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
