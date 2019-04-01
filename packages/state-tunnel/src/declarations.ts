ed2591a73012d550b28a5f7d543731c5934c6951
Adam Bradley <adambradley25@gmail.com>
2019-04-01 17:19:23 -0500

    fix(types): update ConsumerRenderer return type

--- packages/state-tunnel/src/declarations.ts
+++ packages/state-tunnel/src/declarations.ts
@@ -1,3 +1,3 @@
 export type PropList<T> = Extract<keyof T, string>[] | string;
 export type SubscribeCallback<T> = (el: HTMLStencilElement, props: PropList<T>) => () => void;
-export type ConsumerRenderer<T> = (subscribe: SubscribeCallback<T>, renderer: Function) => JSX.Element;
+export type ConsumerRenderer<T> = (subscribe: SubscribeCallback<T>, renderer: Function) => any;
